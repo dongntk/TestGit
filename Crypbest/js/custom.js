71865e58175bb2f2cd74e2d666a157e59a6e24c1
dongntk <kimdongit.code@gmail.com>
2018-09-18 01:36:13 +0700

    up

--- Crypbest/js/custom.js
+++ Crypbest/js/custom.js
@@ -1,7 +1,22 @@
 $(document).ready(function() {
+    $('#activity').DataTable();
 	$('.nav-toggle').click(function() {
         $('#main-wrapper').toggleClass('show-sidebar');
         $(this).find('.icon-close').toggleClass('icon-menu');
         $(this).find('.icon-arrow-right').toggleClass('icon-menu');
     });
+
+
+	$('.btn-buy').click(function(){
+		var tab_id = $(this).attr('data-tab');	
+		$('.upgrade-inform').removeClass('hide');	
+		$('.checkout-cart').removeClass('current');
+		$(this).addClass('current');
+		$("#"+tab_id).addClass('current');
+		$("#"+tab_id).parent().find('.upgrade-inform').addClass('hide');
+	});
+	$('.btn-closebot').click(function(){
+		$('.checkout-cart').removeClass('current');
+		$('.upgrade-inform').removeClass('hide');
+	});
 });
